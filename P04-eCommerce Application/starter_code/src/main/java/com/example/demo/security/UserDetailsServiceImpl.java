7c3651fa86c912cfbfd1b49263876471a7eda074
Your Name <you@example.com>
2019-11-11 17:01:30 -0500

    Splunk TCP connector working Error handling

--- P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ P04-eCommerce Application/starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import static java.util.Collections.emptyList;
 
-//import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.UserRepository;
 
 @Service
@@ -21,13 +20,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        //System.out.println("\n\n###############  loadbyUsername ###########\n\n");
-        //return this.userRepository.findByUsername(username).map(CreateUserRequest::new).orElseThrow
         com.example.demo.model.persistence.User applicationUser = userRepository.findByUsername(username);
         if (applicationUser == null) {
              throw new UsernameNotFoundException(username);
          }
-        //System.out.println("\n\n###############  loadbyUsername return statement ###########\n\n");
         return new User(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
     }
 }
